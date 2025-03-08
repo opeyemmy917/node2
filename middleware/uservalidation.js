3c14f6bc25e717a6cb309487716d2901eb100d48
opeyemmy <olawunmiopeyemi19@gmail.com>
2025-03-08 13:32:40 +0100

    add new

--- middleware/uservalidation.js
+++ middleware/uservalidation.js
@@ -4,7 +4,7 @@ const yup = require("yup")
 const uservalidation = yup.object({
     name:yup.string().min(4,"name is too short").required("name cannot be empty"),
     email:yup.string().email("must be a valid email").required("email is required"),
-    password:yup.string().min(8,"password must not be a minimum of 8 characters".required("password must not be empty"))
+    password:yup.string().min(8,"password must not be a minimum of 8 characters").required("password must not be empty")
 })
 
 module.exports=uservalidation
